f699673f17616170be919b2201b3648094782f19
dulithaprasanna <dulithaprasanna@gmail.com>
2023-03-17 22:28:32 -0500

    updating test_optimizer

--- tests/amlro/test_optimizer.py
+++ tests/amlro/test_optimizer.py
@@ -25,7 +25,7 @@ class TestUniformGrid(unittest.TestCase):
 
         decoded_list = optimizer.categorical_feature_decoding(config,encoded)
 
-        self.assertListEqual(decoded_list.tolist(), decoded)
+        self.assertListEqual(list(decoded_list), decoded)
 
     def test_categorical_feature_encoding(self):
         """Test that a decoded parameter list convert into encoded list."""
@@ -47,4 +47,4 @@ class TestUniformGrid(unittest.TestCase):
 
         encoded_list = optimizer.categorical_feature_encoding(config,decoded)
 
-        self.assertListEqual(encoded_list.tolist(), encoded)
+        self.assertListEqual(list(encoded_list), encoded)
